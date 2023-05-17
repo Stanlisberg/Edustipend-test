91987c1a76d11512adadbeb8c00052e0226d2989
Stanlisberg <nnabugo2010@gmail.com>
2023-05-17 17:29:18 +0100

    Feature: Navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/navbar"
 
 function App() {
   
   return (
     <>
       <div>
-        Hello Edustipend!
+        <Navbar />
       </div>
     </>
   )
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar () {
+    return(
+        <nav className='nav-container'>
+            <div className='nav-wrapper'>
+                <ul className='nav-list'>
+                    <li>Contact Us</li>
+                </ul>
+                <button className='nav-button'>
+                    Signup
+                </button>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
